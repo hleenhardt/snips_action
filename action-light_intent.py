7b3fc4a853f068b40ab777808e229959805aa7be
hleenhardt <hleenhardt@gmail.com>
2019-05-05 22:39:09 +0200

    Ajout du logger

--- action-light_intent.py
+++ action-light_intent.py
@@ -1,25 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
 from hermes_python.hermes import Hermes
+from lights_controller import turn_light_on
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
+logger    = logging.getLogger("Light Intent")
+handler   = logging.FileHandler("/tmp/snips_user_logs", mode="a", encoding="utf-8")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
+handler.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
 
 def intent_received(hermes, intent_message):
-    sentence = "Vous avez demande d'allumer la lumiere "
+    sentence = "J'allume "
 
-    file = open("/tmp/test_log.txt","r")
-    file.write("{}".format(intent_message))
+    file = open("/tmp/test_log.txt","w")
+    file.write("{}\n".format(intent_message))
     file.close()
-    #if intent_message.intent.intent_name == "allumerLaLumiere":
-    #    print("Hello")
-    #else:
-    #    return
+    if "allumerLaLumiere" in intent_message.intent.intent_name:
+        rooms = process_turn_lights_on_intent(intent_message.intent.slots)
+    else:
+        return
 
+    sentence += ",".join(rooms)
     hermes.publish_end_session(intent_message.session_id, sentence)
+
+
+def process_turn_lights_on_intent(slots):
+    """
+    Process slots of the intent to and call the turn°light_on function
+
+    INPUT:
+        - slots <list> : list of all the room for which we need to turn the lights on
+    OUPUT:
+        - rooms <list> : list of room's name
+    """
+    rooms = []
+    for slot in slots:
+        if slot.slotName != "piece":
+            logger.error("Wrong slot type for this intent [slot_type={} ; intent=allumerLaLumiere]".format(slot.slotName))
+        room = slot.value.value
+        rooms.append(room)
+
+    turn_light_on(rooms)
+    return rooms
+
+
 
 
 with Hermes(MQTT_ADDR) as h:
