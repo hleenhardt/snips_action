e997058f52cb880b20a5cc75968878e6d5082753
hleenhardt <hleenhardt@gmail.com>
2019-05-05 21:52:42 +0200

    Test mise en place actions

--- action-light_intent.py
+++ action-light_intent.py
@@ -9,20 +9,19 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
 def intent_received(hermes, intent_message):
-    sentence = "Vous avez demandé d'allumer la lumière "
+    sentence = "Vous avez demande d'allumer la lumiere "
 
-    if intent_message.intent.intent_name == 'allumerLaLumiere':
-        turn_light_on(intent_message)
-    else:
-        return
+    file = open("/tmp/test_log.txt","r")
+    file.write("{}".format(intent_message))
+    file.close()
+    #if intent_message.intent.intent_name == "allumerLaLumiere":
+    #    print("Hello")
+    #else:
+    #    return
 
     hermes.publish_end_session(intent_message.session_id, sentence)
 
 
-def turn_light_on(intent):
-    rooms = intent.slots
-    file = open("/tmp/test_log.txt","r")
-    file.write("{}".format(rooms))
-
 with Hermes(MQTT_ADDR) as h:
+    print("Starting")
     h.subscribe_intents(intent_received).start()
